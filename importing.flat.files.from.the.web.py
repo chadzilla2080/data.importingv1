07906efb79a2cb3d669e85bfea626e9d8836165a
Chad Buie <chadbuie@gmail.com>
2019-03-22 05:57:18 -0400

    Loaded in the code updates and libraries.
    This update includes the resource url, the urlib, request, and urlretrieve function to import the data, make a copy and then view it in a data frame.

---
+++ importing.flat.files.from.the.web.py
@@ -0,0 +1,15 @@
+# Import package
+from ____ import ____
+
+# Import pandas
+import pandas as pd
+
+# Assign url of file: url
+
+
+# Save file locally
+
+
+# Read file into a DataFrame and print its head
+df = pd.read_csv('winequality-red.csv', sep=';')
+print(df.head())
