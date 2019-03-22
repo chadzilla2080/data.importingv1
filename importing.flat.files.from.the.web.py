3b4f0b8e29a80a041649468a04479c64dafc5a80
Chad Buie <chadbuie@gmail.com>
2019-03-22 05:58:51 -0400

    Updated second version of code
    This code features the full code to import using urllib and request.

--- importing.flat.files.from.the.web.py
+++ importing.flat.files.from.the.web.py
@@ -1,14 +1,14 @@
 # Import package
-from ____ import ____
+from urllib.request import urlretrieve
 
 # Import pandas
 import pandas as pd
 
 # Assign url of file: url
-
+url = 'https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
 
 # Save file locally
-
+urlretrieve(url, 'winequality-red.csv')
 
 # Read file into a DataFrame and print its head
 df = pd.read_csv('winequality-red.csv', sep=';')
